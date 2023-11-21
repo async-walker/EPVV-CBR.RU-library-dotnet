0b8184c60a3889c80d30c1b4abb12e1626e750d5
Valeriy <valeriy.elykomov@gmail.com>
2023-11-21 15:56:03 +0500

    Create ReceiptsStatus.cs

---
+++ EPVV_CBR.RU/Data/Enums/ReceiptsStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace EPVV_CBR.RU.Data.Enums
+{
+    public enum ReceiptsStatus
+    {
+        Sent,
+        Delivered,
+        Error,
+        Processing,
+        Registered,
+        Rejected,
+        New,
+        Read,
+        Replied,
+        Success
+    }
+}
