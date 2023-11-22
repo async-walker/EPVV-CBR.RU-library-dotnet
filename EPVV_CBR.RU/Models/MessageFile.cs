088c4bcf5b87fb457b8afc591715a1d52d59ce17
Valeriy <valeriy.elykomov@gmail.com>
2023-11-22 10:23:23 +0500

    Create FileType enum

--- EPVV_CBR.RU/Models/MessageFile.cs
+++ EPVV_CBR.RU/Models/MessageFile.cs
@@ -1,10 +1,12 @@
-﻿namespace EPVV_CBR.RU.Models
+﻿using EPVV_CBR.RU.Data.Enums;
+
+namespace EPVV_CBR.RU.Models
 {
     public class MessageFile
     {
         public string Name { get; set; }
         public string? Description { get; set; }
-        public string FileType { get; set; }
+        public FileType FileType { get; set; }
         public bool Encrypted { get; set; }
         public string? SignedFile { get; set; }
         public long Size { get; set; }
