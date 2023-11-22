5364c9f88e15098d455f4489164d44ca0ac26bc1
Valeriy <valeriy.elykomov@gmail.com>
2023-11-22 16:34:50 +0500

    Create EpvvException.cs

---
+++ EPVV_CBR.RU/Exceptions/EpvvException.cs
@@ -0,0 +1,23 @@
+﻿using EPVV_CBR.RU.Models;
+
+namespace EPVV_CBR.RU.Exceptions
+{
+    /// <summary>
+    /// Репрезентация ошибки взаимодействия с ЕПВВ
+    /// </summary>
+    public class EpvvException : Exception
+    {
+        /// <summary>
+        /// Инициализация ошибки взаимодействия с ЕПВВ
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="errorResponse"></param>
+        public EpvvException(string message, ErrorResponse errorResponse) 
+            : base(message) => ErrorResponse = errorResponse;
+
+        /// <summary>
+        /// Ошибка REST-метода
+        /// </summary>
+        public ErrorResponse ErrorResponse { get; } = default!;
+    }
+}
