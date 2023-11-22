16e4553ca9267fed72493693e5c8d2c4853f240a
Valeriy <valeriy.elykomov@gmail.com>
2023-11-22 11:37:19 +0500

    Create MessageType.cs

---
+++ EPVV_CBR.RU/Data/Enums/MessageType.cs
@@ -0,0 +1,17 @@
+﻿namespace EPVV_CBR.RU.Data.Enums
+{
+    /// <summary>
+    /// Тип сообщения
+    /// </summary>
+    public enum MessageType
+    {
+        /// <summary>
+        /// Исходящее сообщение
+        /// </summary>
+        outbox,
+        /// <summary>
+        /// Входящее
+        /// </summary>
+        inbox
+    }
+}
