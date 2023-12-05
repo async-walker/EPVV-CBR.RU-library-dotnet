47ace96bf28c45df4db74b52080ebc5654c3f89f
Valeriy <valeriy.elykomov@gmail.com>
2023-12-05 11:21:18 +0500

    Change acces for ContentType enum

--- EPVV_CBR.RU/Enums/ContentType.cs
+++ EPVV_CBR.RU/Enums/ContentType.cs
@@ -1,17 +1,8 @@
 ﻿namespace EPVV_CBR.RU.Enums
 {
-    /// <summary>
-    /// Тип передаваемого HTTP-контента
-    /// </summary>
-    public enum ContentType
+    internal enum ContentType
     {
-        /// <summary>
-        /// application/json
-        /// </summary>
         ApplicationJson,
-        /// <summary>
-        /// application/octet-stream
-        /// </summary>
         ApplicationOctetStream
     }
 }
