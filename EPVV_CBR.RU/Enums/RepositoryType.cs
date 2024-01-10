5a1e862a8eee0d84328bdd8c457cb6688c00cf5c
Valeriy <valeriy.elykomov@gmail.com>
2024-01-10 10:27:52 +0500

    Update RepositoryType
    Unknow value from API response - 'filestorage'

--- EPVV_CBR.RU/Enums/RepositoryType.cs
+++ EPVV_CBR.RU/Enums/RepositoryType.cs
@@ -12,6 +12,12 @@
         /// <summary>
         /// Отправка файлов сообщения через FASP (ТПС ASPERA)
         /// </summary>
-        aspera
+        aspera,
+        /// <summary>
+        /// NOT DOCUMENTED IN OFFICIAL DOCUMENTATION REST API CBR.RU
+        /// </summary>
+        filestorage // Этого нет в оф.документации ЦБ.
+                    // Но при получении ответа с конца декабря на тестовом сервере -
+                    // данный тип присутсвует в теле сообщения.
     }
 }
