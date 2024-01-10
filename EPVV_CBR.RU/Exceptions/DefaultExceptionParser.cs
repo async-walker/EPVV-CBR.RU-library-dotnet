4e1b4cfa157cd235255d7a272dbbee92398ec57a
Valeriy <valeriy.elykomov@gmail.com>
2024-01-10 11:45:25 +0500

    Update DefaultExceptionParser.cs

--- EPVV_CBR.RU/Exceptions/DefaultExceptionParser.cs
+++ EPVV_CBR.RU/Exceptions/DefaultExceptionParser.cs
@@ -1,19 +1,16 @@
 ﻿namespace EPVV_CBR_RU.Exceptions
 {
     /// <summary>
-    /// Дефолтная реализация <see cref="IExceptionParser"/>, всегда возвращает <see cref="ApiRequestException"/>
+    /// Реализация <see cref="IExceptionParser"/> по умолчанию. Всегда возвращает <see cref="ApiRequestException"/>
     /// </summary>
     public class DefaultExceptionParser : IExceptionParser
     {
         /// <inheritdoc />
         public ApiRequestException Parse(ApiResponseError apiResponse)
         {
-            if (apiResponse is null)
-            {
-                throw new ArgumentNullException(nameof(apiResponse));
-            }
-
-            return new(
+            return apiResponse is null
+                ? throw new ArgumentNullException(nameof(apiResponse))
+                : new(
                 message: apiResponse.Message,
                 httpStatus: apiResponse.HTTPStatus,
                 code: apiResponse.Code,
