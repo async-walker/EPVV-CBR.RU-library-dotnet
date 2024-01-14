cef4e844ba1d8b78303e585473e12c8dc2c8f901
Valeriy <valeriy.elykomov@gmail.com>
2024-01-14 18:04:21 +0500

    Update EpvvClientExtensions.ApiMethods.cs

--- EPVV_CBR.RU/EpvvClientExtensions.ApiMethods.cs
+++ EPVV_CBR.RU/EpvvClientExtensions.ApiMethods.cs
@@ -1,7 +1,9 @@
-﻿using EPVV_CBR_RU.Extensions;
+﻿using EPVV_CBR_RU.Exceptions;
+using EPVV_CBR_RU.Extensions;
 using EPVV_CBR_RU.Models;
-using EPVV_CBR_RU.Requests.Methods;
-using System.Net.Http.Headers;
+using EPVV_CBR_RU.Requests.Methods.GetMessagesInfo;
+using EPVV_CBR_RU.Requests.Methods.SendMessages;
+using System.Net;
 
 namespace EPVV_CBR_RU
 {
@@ -10,6 +12,19 @@ namespace EPVV_CBR_RU
     /// </summary>
     public static class EpvvClientExtensions
     {
+        /// <summary>
+        /// Создание черновика сообщения
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="task"></param>
+        /// <param name="title"></param>
+        /// <param name="text"></param>
+        /// <param name="files"></param>
+        /// <param name="correlationId"></param>
+        /// <param name="groupId"></param>
+        /// <param name="receivers"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<DraftMessage> CreateDraftMessageAsync(
             this IEpvvClient client,
             string task,
@@ -33,6 +48,14 @@ namespace EPVV_CBR_RU
                     cancellationToken)
                 .ConfigureAwait(false);
 
+        /// <summary>
+        /// Создание сессии загрузки файла
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="messageId"></param>
+        /// <param name="fileId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<SessionInfo> CreateUploadSessionAsync(
             this IEpvvClient client,
             string messageId,
@@ -44,17 +67,59 @@ namespace EPVV_CBR_RU
                    cancellationToken)
                .ConfigureAwait(false);
 
+        /// <summary>
+        /// Загрузка файла на сервер
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="sessionInfo"></param>
+        /// <param name="stream"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<UploadedFile> UploadFileAsync(
             this IEpvvClient client,
             SessionInfo sessionInfo,
-            string filePath,
+            Stream stream,
             CancellationToken cancellationToken = default) =>
             await client.ThrowIfNull()
                .MakeRequestAsync(
-                   request: new UploadFileRequest(sessionInfo, filePath),
+                   request: new UploadFileRequest(sessionInfo, stream),
                    cancellationToken)
                .ConfigureAwait(false);
 
+        /// <summary>
+        /// Подтверждение отправки сообщения
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="messageId"></param>
+        /// <param name="cancellationToken"></param>
+        public static async Task ConfirmSendMessage(
+            this IEpvvClient client,
+            string messageId,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await client.ThrowIfNull()
+                   .MakeRequestAsync(
+                       request: new ConfirmSendMessageRequest(messageId),
+                       cancellationToken)
+                   .ConfigureAwait(false);
+            }
+            catch (RequestException ex)
+            {
+                if (ex.HttpStatusCode is HttpStatusCode.OK)
+                    return;
+                else throw;
+            }
+        }
+
+        /// <summary>
+        /// Поиск сообщений по фильтрам
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="filters"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<List<MessageInfo>> GetMessagesInfoByFiltersAsync(
             this IEpvvClient client,
             QueryFilters? filters = default,
@@ -65,6 +130,13 @@ namespace EPVV_CBR_RU
                    cancellationToken)
                .ConfigureAwait(false);
 
+        /// <summary>
+        /// Поиск информации о сообщении по его ID
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="messageId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<MessageInfo> GetMessageInfoByIdAsync(
             this IEpvvClient client,
             string messageId,
@@ -74,5 +146,30 @@ namespace EPVV_CBR_RU
                    request: new GetMessageInfoByIdRequest(messageId),
                    cancellationToken)
                .ConfigureAwait(false);
+
+        /// <summary>
+        /// Загрузка сообщения
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="messageId"></param>
+        /// <param name="directoryToSave"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<string> DownloadMessageAsync(
+            this IEpvvClient client,
+            string messageId,
+            string directoryToSave,
+            CancellationToken cancellationToken = default)
+        {
+            var path = Path.Combine(directoryToSave, $"{messageId}.zip");
+
+            await client.DownloadFileAsync(
+                endpoint: $"messages/{messageId}/download",
+                destination: new FileStream(path, FileMode.OpenOrCreate),
+                cancellationToken)
+                .ConfigureAwait(false);
+
+            return path;
+        }
     }
 }
