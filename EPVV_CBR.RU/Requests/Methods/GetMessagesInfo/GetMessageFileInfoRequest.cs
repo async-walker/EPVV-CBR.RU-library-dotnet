9ca49c7df4da5b70f139a3f31629710720d689ac
Valeriy <valeriy.elykomov@gmail.com>
2024-01-15 12:52:37 +0500

    Create GetMessageFileInfoRequest.cs

---
+++ EPVV_CBR.RU/Requests/Methods/GetMessagesInfo/GetMessageFileInfoRequest.cs
@@ -0,0 +1,14 @@
+﻿using EPVV_CBR_RU.Types;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace EPVV_CBR_RU.Requests.Methods.GetMessagesInfo
+{
+    [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class GetMessageFileInfoRequest : RequestBase<UploadedFile>
+    {
+        public GetMessageFileInfoRequest(string messageId, string fileId) 
+            : base(HttpMethod.Get, $"messages/{messageId}/files/{fileId}")
+        { }
+    }
+}
