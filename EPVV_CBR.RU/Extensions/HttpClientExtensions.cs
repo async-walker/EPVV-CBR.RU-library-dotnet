3bd3ba2ea200b679a55918f95a54558d6f6b703b
Valeriy <valeriy.elykomov@gmail.com>
2024-01-16 12:18:40 +0500

    Create HttpClientExtensions.cs

---
+++ EPVV_CBR.RU/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace EPVV_CBR_RU.Extensions
+{
+    internal static class HttpClientExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static async Task<HttpResponseMessage> GetResponseAsync(
+            this HttpClient httpClient,
+            HttpRequestMessage requestMessage,
+            CancellationToken cancellationToken)
+        {
+            HttpResponseMessage? httpResponse;
+
+            try
+            {
+                httpResponse = await httpClient
+                    .SendAsync(requestMessage, cancellationToken)
+                    .ConfigureAwait(false);
+            }
+            catch (TaskCanceledException exception)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    throw;
+
+                throw new Exception(
+                    message: "Время запроса вышло",
+                    innerException: exception);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(
+                    message: "Исключение возникло при выполнении запроса",
+                    innerException: exception);
+            }
+
+            return httpResponse;
+        }
+    }
+}
